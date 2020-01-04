b256e1454cdba19da7f29a07c6db46a288be85fd
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2020-01-04 20:25:46 +0100

    Migrated to AndroidX (?) + Firebase-App sign-in with email + password

--- myanimepal/lib/main.dart
+++ myanimepal/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyAnimePal());
 
@@ -8,8 +10,25 @@ class MyAnimePal extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Anime Pal',
-      home: Scaffold
-      (
+      home: SingIn()
+     
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget
+{
+   FirstPage(); 
+
+  @override
+  FirstPageState createState() => FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage>
+{
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold (
         appBar: AppBar
         (
           title: Text("MyAnimePal"),
@@ -51,7 +70,102 @@ class MyAnimePal extends StatelessWidget {
               ); 
           },
         ),
-      ),
-    );
+      ); 
   }
+
+}
+
+class SingIn extends StatefulWidget
+{
+@override
+  SingInState createState() => SingInState();
+}
+
+
+class SingInState extends State<SingIn>
+{
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>(); 
+  String email, password; 
+
+ @override
+  Widget build(BuildContext context) 
+  {
+     return Scaffold (
+        appBar: AppBar
+        (
+          title: Text("Login to MyAnimePal"),
+        ),
+        body: Form
+        (
+          key: formKey,
+          child: Column
+        (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>
+          [
+            TextFormField
+            (
+              validator: (input)
+              {
+                if(input.isEmpty)
+                {
+                  return "Please try again"; 
+                }
+              },
+              onSaved: (input) {
+                email = input; 
+              },
+              decoration: InputDecoration( labelText: "Enter your e-mail", hintText: "example@gmail.com"),
+              style: TextStyle(fontSize: 20),
+            ),
+
+            TextFormField
+            (
+              validator: (input)
+              {
+                if(input.isEmpty)
+                {
+                  return "Please try again"; 
+                }
+              },
+              onSaved: (input) {
+                password = input; 
+              },
+              decoration: InputDecoration( labelText: "Enter your password"),
+              style: TextStyle(fontSize: 20),
+              obscureText: true
+            ),
+
+            RaisedButton
+            (
+              onPressed: _signIn,
+              child: Text("Sign in"),
+            )
+
+          ],
+
+        )
+        )
+     ); 
+
+  }
+
+  Future<void> _signIn() async
+  {
+    FormState state = formKey.currentState; 
+    if(state.validate())
+    {
+      state.save();
+      try
+      {
+         await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password:  password); 
+         Navigator.of(context).push(MaterialPageRoute(builder: (context) => FirstPage())); 
+      }
+      catch(e)
+      {
+
+      }
+    }
+  }
+  
 }
