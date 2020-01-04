1e23eb328bd954818ea55130b789637b52226f08
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2020-01-04 14:27:14 +0100

    Already displaying some demo Firebase real-time data in the Emulator

--- myanimepal/lib/main.dart
+++ myanimepal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyAnimePal());
@@ -7,7 +8,38 @@ class MyAnimePal extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Anime Pal',
-      home: Scaffold(),
+      home: Scaffold
+      (
+        appBar: AppBar
+        (
+          title: Text("MyAnimePal"),
+        ),
+        body: StreamBuilder
+        (
+          stream: Firestore.instance.collection("animes").snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot)
+          {
+            if(!snapshot.hasData)
+            {
+              return Center(child: CircularProgressIndicator());
+            }
+             return ListView.builder
+              (
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index)
+                {
+                  Map<String, dynamic> data = snapshot.data.documents[index].data; 
+                  return ListTile
+                  (
+                    title: Text(data["Genre"], textScaleFactor:  1.3,),
+                    leading: Text("Mean Score: " + data["Mean Score"].toString(), textScaleFactor:  1.3,),
+                  ); 
+                },
+
+              ); 
+          },
+        ),
+      ),
     );
   }
 }
