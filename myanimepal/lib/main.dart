adc6dac72c1452fef0eaeac9fc788ce313621992
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2020-01-04 13:59:59 +0100

    Synch with firebase :fire:

---
+++ myanimepal/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyAnimePal());
+
+class MyAnimePal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Anime Pal',
+      home: Scaffold(),
+    );
+  }
+}
