47115e619f21a8b5e4c2907c220866ad2059b8cb
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2020-01-09 17:30:22 +0100

    Anime/manga description page template

--- myanimepal/lib/DescriptionPage.dart
+++ myanimepal/lib/DescriptionPage.dart
@@ -5,29 +5,94 @@ import 'package:flutter/material.dart';
 class DescriptionPage extends StatefulWidget {
   FirebaseUser user;
   DocumentSnapshot aniManga;
-  DescriptionPage({@required this.user, @required this.aniManga});
+  bool isAnime; 
+  DescriptionPage({@required this.user, @required this.aniManga})
+  {
+    String path = aniManga.reference.path.toString(); 
+    String discriminator = path.substring(0, path.indexOf("/")); 
+    isAnime = (discriminator == 'animes'); 
+  }
 
   @override
   DescriptionPageState createState() => DescriptionPageState();
 }
 
 class DescriptionPageState extends State<DescriptionPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      iconTheme: IconThemeData(
-        color: Colors.black,
-      ),
-      title: Text(
-        widget.aniManga.documentID,
-        style: TextStyle(color: Colors.black, fontSize: 15),
-      ),
-      backgroundColor: Colors.white,
-      actions: <Widget>[
-        Image.network(
-            "https://firebasestorage.googleapis.com/v0/b/myanimepal.appspot.com/o/MyAnimePalLogo.png?alt=media&token=57926b6e-1808-43c8-9d99-e4b5572ef93e")
-      ],
-    ));
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          title: Text(
+            widget.aniManga.documentID,
+            style: TextStyle(color: Colors.black, fontSize: 15),
+          ),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            Image.network(
+                "https://firebasestorage.googleapis.com/v0/b/myanimepal.appspot.com/o/MyAnimePalLogo.png?alt=media&token=57926b6e-1808-43c8-9d99-e4b5572ef93e")
+          ],
+        ),
+        body: infoWidget());
+  }
+
+  infoWidget() {
+    return Container(
+      padding: const EdgeInsets.all(35.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(widget.aniManga.data["ImagePath"]),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText:
+                          "Ep/Cpt's completed"), // TODO: update this in firebase
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                //child: // TODO: Dropdown with possible anime statuses
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText:
+                          "Score"), // TODO: update this in firebase
+                  keyboardType: TextInputType.number,
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              widget.aniManga.data["Description"].toString(),
+              // textScaleFactor: 2,
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
--- myanimepal/lib/main.dart
+++ myanimepal/lib/main.dart
@@ -1,46 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'LoginPage.dart'; 
+import 'LoginPage.dart';
 
+_loadAnimeData() async {
+  QuerySnapshot animesSnapshot =
+      await Firestore.instance.collection("animes").getDocuments();
+  return animesSnapshot.documents;
+}
 
- _loadAnimeData() async
- {
-  QuerySnapshot animesSnapshot = await Firestore.instance.collection("animes").getDocuments(); 
-  return animesSnapshot.documents; 
- }
+_loadMangaData() async {
+  QuerySnapshot mangasSnapshot =
+      await Firestore.instance.collection("mangas").getDocuments();
+  return mangasSnapshot.documents;
+}
 
-  _loadMangaData() async
- {
-  QuerySnapshot mangasSnapshot = await Firestore.instance.collection("mangas").getDocuments(); 
-  return mangasSnapshot.documents; 
- }
-
-
-void main() async
-{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  var animeData = await _loadAnimeData(); 
-  var mangaData = await _loadMangaData(); 
+  var animeData = await _loadAnimeData();
+  var mangaData = await _loadMangaData();
   runApp(MyAnimePal(animeData: animeData, mangaData: mangaData));
-} 
- 
+}
 
 class MyAnimePal extends StatelessWidget {
-
-  List<DocumentSnapshot> animeData, mangaData; 
-  MyAnimePal({@required this.animeData, @required this.mangaData})
-  {
-    // TODO: check the performance of this :) What about 1000+ animes and mangas ??? 
-    animeData.sort((s1, s2) => s1.data["Genre"].toString().compareTo(s2.data["Genre"].toString()));
-    mangaData.sort((s1, s2) => s1.data["Genre"].toString().compareTo(s2.data["Genre"].toString()));
+  List<DocumentSnapshot> animeData, mangaData;
+  MyAnimePal({@required this.animeData, @required this.mangaData}) {
+    // TODO: check the performance of this :) What about 1000+ animes and mangas ???
+    // Why not adding them to the database already ordered by genre?? XDD
+    animeData.sort((s1, s2) =>
+        s1.data["Genre"].toString().compareTo(s2.data["Genre"].toString()));
+    mangaData.sort((s1, s2) =>
+        s1.data["Genre"].toString().compareTo(s2.data["Genre"].toString()));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Anime Pal',
-      home: SingIn(animeData: animeData, mangaData: mangaData)
-     
-    );
+        title: 'My Anime Pal',
+        home: SingIn(animeData: animeData, mangaData: mangaData));
   }
 }
